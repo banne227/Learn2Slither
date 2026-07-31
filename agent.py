c17153b0278e6c53a479f99beb27814b172b930c
banne227 <baidyanne9@gmail.com>
2026-07-31 13:45:36 +0200

    continue to set the norm

--- agent.py
+++ agent.py
@@ -6,7 +6,11 @@ from qlearning import choose_action, update_q
 from qlearning import action_to_delta, load_model, save_model
 
 
-def run(nb_sessions, load_path, save_path, dont_learn, visual, step_by_step, speed):
+def run(
+    nb_sessions, load_path, save_path,
+    dont_learn, visual, step_by_step, speed
+):
+
     Q = load_model(load_path) if load_path else {}
     epsilon = 0.01 if dont_learn else 0.9  # exploitation pure si dont_learn
     when, max_score = 0, 0
@@ -35,7 +39,15 @@ def run(nb_sessions, load_path, save_path, dont_learn, visual, step_by_step, spe
             next_state = board.get_vision()
 
             if not dont_learn:
-                update_q(Q, state, action, reward, next_state, alpha=0.1, gamma=0.9)
+                update_q(
+                    Q,
+                    state,
+                    action,
+                    reward,
+                    next_state,
+                    alpha=0.1,
+                    gamma=0.9,
+                )
 
             state = next_state
 
@@ -44,12 +56,16 @@ def run(nb_sessions, load_path, save_path, dont_learn, visual, step_by_step, spe
                     input("Appuie sur Entrée pour continuer...")
                 else:
                     time.sleep(speed)
-            
+
             if board.score > max_score:
                 when = session
                 max_score = board.score
         epsilon = max(0.05, epsilon * 0.995)
 
-    print(f"Meilleur score atteint : {max_score} à la session {when + 1}/{nb_sessions}")
+    print(
+        f"Meilleur score atteint : {max_score} "
+        f"à la session {when + 1}/{nb_sessions}"
+    )
+
     if save_path:
         save_model(Q, save_path)
